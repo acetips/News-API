3fe866bdfd55575f3a49d5a1a7a40911891f8ffa
emelda <YOUR EMAIL ADDRESS>
2021-10-31 16:10:01 +0300

    get api key and article URL.

--- app/request.py
+++ app/request.py
@@ -1,4 +1,13 @@
 from app import app
+import urllib.request,json
+
+from app.article_test import Article
+from .models import article
+
+Article = article.Article
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the artile base url
+base_url = app.config['ARTICLE_API_BASE_URL']
